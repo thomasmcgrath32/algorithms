0ae61b03a5fb4eefbd7a6826622aeea04eb28139
thomasmcgrath32 <36748052+thomasmcgrath32@users.noreply.github.com>
2024-04-23 16:02:42 -0400

    Optimized user data handling, reduced amount of scanners, and condensed methods.

--- src/App.java
+++ src/App.java
@@ -2,41 +2,35 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        add();
-        subtract();
-
-    }
-
-    static void add() {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter X:");
         int x = Integer.parseInt(in.nextLine());
         System.out.println("Enter Y:");
         int y = Integer.parseInt(in.nextLine());
+        System.out.println("Addition: " + add(x,y));
+        System.out.println("Subtraction: " + subtract(x,y));
+        System.out.println("GCF: " + GCF(x,y));
+        in.close();
+    }
 
+    static int add(int x, int y) {
         int total = x + y;
-        System.out.println(total);
+        return total;
     }
 
-    static void subtract() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter X:");
-        int x = Integer.parseInt(in.nextLine());
-        System.out.println("Enter Y:");
-        int y = Integer.parseInt(in.nextLine());
-
+    static int subtract(int x, int y) {
         int total = x - y;
-        System.out.println(total);
+        return total;
     }
     
-    static void GCF() {
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter X:");
-        int x = Integer.parseInt(in.nextLine());
-        System.out.println("Enter Y:");
-        int y = Integer.parseInt(in.nextLine());
-
-        
+    static int GCF(int x, int y) {
+        int GCF = 0; 
+        for(int i = 1; i <= x && i <= y; i++) {
+            if(x % i == 0 && y % i == 0) {
+                GCF = i;
+            }
+        }
+        return GCF;    
     }
+    
 }
